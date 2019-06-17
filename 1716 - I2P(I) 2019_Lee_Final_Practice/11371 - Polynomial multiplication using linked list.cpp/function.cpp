de2aa2b4d390cb657c122e93a373d4f95dee83b4
mimiliaogo <44743062+mimiliaogo@users.noreply.github.com>
2019-06-17 15:04:18 +0800

    second practice

--- 1716 - I2P(I) 2019_Lee_Final_Practice/11371 - Polynomial multiplication using linked list.cpp/function.cpp
+++ 1716 - I2P(I) 2019_Lee_Final_Practice/11371 - Polynomial multiplication using linked list.cpp/function.cpp
@@ -1,75 +1,65 @@
 #include <iostream>
 #include "function.h"
 using namespace std;
-Node* makenode(long data, int power)
+Node* newNode(int data, int power)
 {
-    Node* node;
-    node = new Node;
-    node->data = data;
-    node->power = power;
-    node->next = nullptr;
-    node->prev = nullptr;
-    return node;
+    Node* np = new Node;
+    np->data = data;
+    np->power = power;
+    np->next = np->prev = nullptr;
+    return np;
 }
 Node* create()
 {
-    Node* head = nullptr;
+    int n;
     long data;
     int power;
     cin>>data>>power;
-    head = makenode(data, power);
-    if (power==0) return head;
-    else {
+    Node* head = newNode(data, power);
+    if (power!=0) {
         head->next = create();
         head->next->prev = head;
-        return head;
     }
-
+    return head;
 }
 void printNode(Node* head)
 {
-    if (head==nullptr) return;
-    else {
-        if (head->data!=0L)
-            cout<<' '<<head->data<<' '<<head->power;
+    if (head) {
+        if (head->data!=0) cout<<' '<<head->data<<' '<<head->power;
         printNode(head->next);
     }
+
 }
 Node* multiple(Node* p1, Node* p2)
 {
     Node* p3=nullptr;
-    long data;
-    int power;
-    Node*np;
-    Node* tmp3=nullptr,  *tmp3last;
-    for (Node* tmp2=p2; tmp2!=nullptr; tmp2=tmp2->next) {
-        for (Node* tmp1=p1; tmp1!=nullptr; tmp1=tmp1->next) {
-            data = tmp2->data * tmp1->data;
-            power = tmp2->power + tmp1->power;
-            if (p3==nullptr) p3 = makenode(data, power);
+    Node* tmp4;
+    for(Node*tmp2=p2; tmp2!=nullptr; tmp2=tmp2->next) {
+        for (Node*tmp1=p1; tmp1!=nullptr; tmp1=tmp1->next) {
+            long data = tmp2->data * tmp1->data;
+            int power = tmp2->power+tmp1->power;
+            if (p3==nullptr) p3 = newNode(data, power);
             else {
+                Node*tmp3;
                 for (tmp3=p3; tmp3!=nullptr; tmp3=tmp3->next) {
-                    tmp3last = tmp3;
-                    if (power<tmp3->power) {}//doing nothing
-                    else {//> or ==
-                        if (power==tmp3->power) {
-                            tmp3->data += data;
-                        } else {
-                            np = makenode(data, power);
-                            tmp3->prev->next = np;
-                            np->prev = tmp3->prev;
-                            np->next = tmp3;
-                            tmp3->prev = np;
-                        }
+                    tmp4 = tmp3;
+                    if (power==tmp3->power) {
+                        tmp3->data +=data;
+                        break;
+                    } else if (power>tmp3->power){
+                        Node* np = newNode(data, power);
+                        np->next = tmp3;
+                        np->prev = tmp3->prev;
+                        tmp3->prev->next = np;
+                        tmp3->prev = np;
                         break;
                     }
                 }
                 if (tmp3==nullptr) {
-                    np = makenode(data, power);
-                    tmp3last->next = np;
-                    np->prev = tmp3last;
-                    np->next = nullptr;
+                    tmp4->next = newNode(data, power);
+                    tmp4->next->prev = tmp4;
                 }
+
             }
         }
     }
@@ -77,7 +67,7 @@ Node* multiple(Node* p1, Node* p2)
 }
 void destroy(Node *node)
 {
-    if (node!=nullptr) {
+    if (node) {
         destroy(node->next);
         delete node;
     }
